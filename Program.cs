ac997b2725a06bb86d906714c64e4e9124149e57
Brian Bocquet <brianbocquet@gmail.com>
2023-02-25 08:24:09 +0100

    Console app created

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D15_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
