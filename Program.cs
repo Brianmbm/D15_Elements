885b1173c983dd8ce27e28ad29ae2dde75be8c91
Brian Bocquet <brianbocquet@gmail.com>
2023-02-25 08:44:20 +0100

    Added element class, initialized three elements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace D15_Elements
 {
+    
     internal class Program
     {
+        class Element
+        {
+            public string name, type;
+            public int Z; 
+            public double meltingPoint, boilingPoint; 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Element oxygen = new Element{ name = "Oxygen", Z = 8, type = "non-metal", meltingPoint = 54.36, boilingPoint = 90.188};
+            Element iron = new Element { name = "Iron", Z = 26, type = "metal", meltingPoint = 1811, boilingPoint = 3134 };
+            Element gold = new Element { name = "Gold", Z = 79, type = "metal", meltingPoint = 1337.33, boilingPoint = 3243 };
+
         }
     }
 }
